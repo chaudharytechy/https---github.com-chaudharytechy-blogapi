3c9970649fd78e917150ce47ec4161ba57e06f04
chaudharytechy <amitchaud10@gmail.com>
2023-05-05 10:12:53 +0530

    update

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const router=require('./routes/web')
 
 
 const app = express()
-const port = process.env.PORT
+const port = 5000
 // const cloud=process.env.CLOUDINARY_API_SECRET
 // console.log(cloud)
 
@@ -31,10 +31,10 @@ connectDB()
 
 
 cloudinary.config({
-  cloud_name:process.env.CLOUDINARY_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret:process.env.CLOUDINARY_API_SECRET
-  // secure: true
+  cloud_name: 'dpnselvgk',
+    api_key: '168413136638247',
+    api_secret: 'rnJw7csTkm-mOgNrIYRUohJmNJ0',
+    // secure: true
 });
 // body parser
 // app.use(bodyParser.urlencoded({ extended: false }))
--- db/connect_db.js
+++ db/connect_db.js
@@ -1,7 +1,7 @@
 const mongoose=require('mongoose')
 
 const connectDB=()=>{
-    return mongoose.connect(process.env.DB_URL)
+    return mongoose.connect("mongodb+srv://amit8601396382:Amit123@cluster0.o6b8rav.mongodb.net/?retryWrites=true&w=majority")
     .then(()=>{
         console.log('connection successfully')
     })
